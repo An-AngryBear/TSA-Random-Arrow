b4d4727107e02c375e17d4041171a8a08e3a3c42
An-AngryBear <monahajt@gmail.com>
2017-06-19 22:29:35 -0500

    build random number gen

---
+++ random.js
@@ -0,0 +1,13 @@
+var container = document.getElementById("container");
+
+window.addEventListener("click", function() {
+	let coinFlip = Math.round(Math.random());
+	console.log(coinFlip);
+	container.classList = "";
+	if (coinFlip === 1) {
+		container.classList.add("left-arrow")
+	} else {
+		container.classList.add("right-arrow")
+
+	}
+})
